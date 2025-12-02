30b0f545ef63f16dae7b3f01f2ef14b126de6fb8
Courage <cocretard03@gmail.com>
2025-12-03 02:33:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from flask import Flask
 # -----------------------------------
 # Configuration
 # -----------------------------------
-SD_MESSAGES = ["sD", "sd", "Sd", "SD"]        # Channel 1 messages
+SD_MESSAGES = ["lalala", "lala", "la", "lalala"]        # Channel 1 messages
 NS_MESSAGES = ["NS", "ns", "Ns", "nS"]        # Channel 2 messages
 
 SD_MIN = 490
